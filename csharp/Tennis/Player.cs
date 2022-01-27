962c413394db047ea4e77dbc1a1894a934ae28dd
Kilian Suárez <ksuarez@domingoalonsogroup.com>
2022-01-27 17:55:06 +0000

    delete

--- csharp/Tennis/Player.cs
+++ csharp/Tennis/Player.cs
@@ -1,10 +1,8 @@
 namespace Tennis {
     public class Player {
-        private static int score2;
         private int score;
 
         public Player(int score) {
-            score2 = 1;
             this.score = score;
         }
 

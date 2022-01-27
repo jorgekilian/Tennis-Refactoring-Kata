309ee743a03203027b071b827da9487fb3f53de5
Kilian Suárez <ksuarez@domingoalonsogroup.com>
2022-01-27 18:07:00 +0000

    make public

--- csharp/Tennis/Player.cs
+++ csharp/Tennis/Player.cs
@@ -1,9 +1,11 @@
 namespace Tennis {
     public class Player {
         public int Score { get; private set; }
+        public string Name { get; }
 
         public Player(int score, string name) {
             Score = score;
+            Name = name;
         }
 
         public void AddScore() {

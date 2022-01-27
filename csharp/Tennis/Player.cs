e597a04eb5ea62d0f4464d1c28aecd20063e1756
Kilian Suárez <ksuarez@domingoalonsogroup.com>
2022-01-27 17:30:55 +0000

    introduce field

--- csharp/Tennis/Player.cs
+++ csharp/Tennis/Player.cs
@@ -1,12 +1,15 @@
 namespace Tennis {
     public class Player {
         private TennisGame1 tennisGame1;
+        private int score;
+
         public Player(TennisGame1 tennisGame1) {
             this.tennisGame1 = tennisGame1;
         }
 
         public int AddScore() {
-            return tennisGame1.m_score1 += 1;
+            score = tennisGame1.m_score1 += 1;
+            return score;
         }
     }
 }

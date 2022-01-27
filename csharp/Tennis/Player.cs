5baf172779ca73f19161eb8d6f2d4ecd8c894aec
Kilian Suárez <ksuarez@domingoalonsogroup.com>
2022-01-27 17:45:43 +0000

    manual cambiar uso de una variable por otra

--- csharp/Tennis/Player.cs
+++ csharp/Tennis/Player.cs
@@ -1,15 +1,16 @@
 namespace Tennis {
     public class Player {
         private static int score2;
-        private readonly int score;
+        private int score;
 
         public Player(int score) {
             score2 = 1;
             this.score = score;
         }
 
-        public static int AddScore() {
-            return score2;
+        public int AddScore() {
+            this.score += 1;
+            return this.score;
         }
     }
 }

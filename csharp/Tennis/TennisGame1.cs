28298732b0c84b210d6fb810c5962cd39af9d41d
Kilian Suárez <ksuarez@domingoalonsogroup.com>
2022-01-28 09:17:28 +0000

    delete first switch

--- csharp/Tennis/TennisGame1.cs
+++ csharp/Tennis/TennisGame1.cs
@@ -1,63 +1,41 @@
-namespace Tennis
-{
-    public class TennisGame1 : ITennisGame
-    {
+using System.Collections.Generic;
+
+namespace Tennis {
+    public class TennisGame1 : ITennisGame {
         private readonly Player player1;
         private readonly Player player2;
+        private List<string> scores = new List<string> { "Love-All", "Fifteen-All", "Thirty-All" };
 
-        public TennisGame1(string player1Name, string player2Name)
-        {
+        public TennisGame1(string player1Name, string player2Name) {
             player1 = Player.CreatePlayer(0, player1Name);
             player2 = Player.CreatePlayer(0, player2Name);
         }
 
-        public void WonPoint(string playerName)
-        {
+        public void WonPoint(string playerName) {
             if (playerName == player1.Name)
                 player1.AddScore();
             else
                 player2.AddScore();
         }
 
-        public string GetScore()
-        {
+        public string GetScore() {
             string score = "";
             var tempScore = 0;
-            if (player1.Score == player2.Score)
-            {
-                switch (player1.Score)
-                {
-                    case 0:
-                        score = "Love-All";
-                        break;
-                    case 1:
-                        score = "Fifteen-All";
-                        break;
-                    case 2:
-                        score = "Thirty-All";
-                        break;
-                    default:
-                        score = "Deuce";
-                        break;
-
-                }
+            if (player1.Score == player2.Score) {
+                score = player1.Score > 2 ? "Deuce" : scores[player1.Score];
             }
-            else if (player1.Score >= 4 || player2.Score >= 4)
-            {
+            else if (player1.Score >= 4 || player2.Score >= 4) {
                 var minusResult = player1.Score - player2.Score;
                 if (minusResult == 1) score = "Advantage player1";
                 else if (minusResult == -1) score = "Advantage player2";
                 else if (minusResult >= 2) score = "Win for player1";
                 else score = "Win for player2";
             }
-            else
-            {
-                for (var i = 1; i < 3; i++)
-                {
+            else {
+                for (var i = 1; i < 3; i++) {
                     if (i == 1) tempScore = player1.Score;
                     else { score += "-"; tempScore = player2.Score; }
-                    switch (tempScore)
-                    {
+                    switch (tempScore) {
                         case 0:
                             score += "Love";
                             break;
